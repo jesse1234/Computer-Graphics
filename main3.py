fb70fcb1a9f197505fe4d33ac7f561c152b07d07
OtaoDavis <davis.otao@strathmore.edu>
2023-10-06 06:57:02 +0300

    zipping and uploading function

--- main3.py
+++ main3.py
@@ -1,6 +1,7 @@
 import os
 import argparse
-from functions import create_excel_file, load_en_us_data, extract_data_from_jsonl
+import json
+from functions import create_excel_file, load_en_us_data, extract_data_from_jsonl, zip_excel_files, google_drive_upload
 
 def main():
     parser = argparse.ArgumentParser(description='Convert JSONL files to Excel files by language')
@@ -11,7 +12,10 @@ def main():
     '''These are the input and output folder paths'''
     input_folder_path = args.input_folder
     output_folder_path = args.output_folder
+    input_excel_path = args.input_folder
+    output_zip_path = args.output_folder
     os.makedirs(output_folder_path, exist_ok=True)
+    os.makedirs(output_zip_path, exist_ok=True)
 
     '''Load en-US data for reference'''
     en_us_file_path = os.path.join(input_folder_path, "en-US.jsonl")
@@ -26,6 +30,23 @@ def main():
             '''Create Excel files for each language'''
             for language, data in data_by_language.items():
                 create_excel_file(language, data, output_folder_path)
+
+                input_excel_path = "output_excel_files"
+    zip_file_name = "output.zip"
+
+    zip_excel_files(input_excel_path, output_zip_path, zip_file_name)
+
+    ''' Post the zip file to Google Drive '''
+    access_token = "ya29.a0AfB_byBKVEKCr0LYnhK9dg9Av0ZsTKgYaGWsaTQ7MSdiI4v9FpdciAODAD-emsAqq_yFlWnHgu7tMdLrXXRT_5j5YYMXP6C92WpaNU9Sage2DpvG3UsEw7CRNsKMllwyVW3fZaEofazE9c1guHAlPKO8ABiwU7j2ir6AaCgYKAWcSARISFQGOcNnCI68Urgw2tjP5KF71p7Ah4w0171"
+    upload_file_path = "output_excel_files/output.zip"
+    output_file_name = "files.zip"
+    
+    file_id = google_drive_upload(access_token, upload_file_path, output_file_name)
+    
+    if file_id:
+        print("Zip file uploaded to Google Drive")
+    else:
+        print("Zip file not uploaded to Google Drive")  
 
 if __name__ == '__main__':
     main()
